2cf3396f33b62eff54f9585a8af736dbdb434098
RanaUniverse <RanaUniverse321@gmail.com>
2025-04-25 00:06:04 +0530

    some basic url funcions working

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-print("Rana Universe")
+# save this as app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+
+my_name = "Rana Universe"
+
+
+@app.route("/")
+def index():
+    return "Index Page"
+
+
+@app.route("/hello")
+def hello():
+    return "Hello, World"
+
+
+
+from markupsafe import escape
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User Name is: {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post ID IS: {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath  file relateive is: {escape(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
